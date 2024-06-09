9c2c3f54f0c81cdf8b124ccccbe3f47df3abe534
Nieze Ben Mansour <nieze.benmansour@outlook.fr>
2024-06-09 22:26:13 +0100

    Add demo 3

---
+++ Demos/Nieze.WPF.Demos.Demo3/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Nieze.WPF.Demos.Demo3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var windowExemple = new WindowExemple();
+            windowExemple.Show();
+        }
+    }
+}
---
+++ Demos/Nieze.WPF.Demos.Demo3/WindowExemple.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Nieze.WPF.Demos.Demo3
+{
+    /// <summary>
+    /// Interaction logic for WindowExemple.xaml
+    /// </summary>
+    public partial class WindowExemple : Window
+    {
+        public WindowExemple()
+        {
+            InitializeComponent();
+        }
+
+        public void SetTrue(object sender, RoutedEventArgs e) 
+        {
+            Grid.SetIsSharedSizeScope(dp1, true);
+            txt1.Text = "IsSharedSizeScope Property is set to " + Grid.GetIsSharedSizeScope(dp1).ToString();
+        }
+
+        public void SetFalse(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
